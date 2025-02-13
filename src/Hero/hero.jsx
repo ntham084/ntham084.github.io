fdf968fe6257b7e083452f36414cdef76a061a64
ntham084 <nthamtoro@gmail.com>
2025-02-13 02:17:15 -0800

    Feat: Added text for a biography and 4 buttons that will lead to professional social media

--- src/Hero/hero.jsx
+++ src/Hero/hero.jsx
@@ -7,6 +7,22 @@ const Hero = () => {
     return (
         <div id = "Home" className = 'hero'>
             <img src= {profile_img} alt="Profile Picture of Nathan" />
+            <h1><span>I'm Nathan Thamtoro,</span> Computer Science Student from Simon Fraser University</h1>
+            <p>I am a aspiring software engineer ready to make the best of any oppurtunity given</p>
+            <div className = "hero-action">
+                <a href="https://github.com/ntham084" target="_blank" rel="noopener noreferrer" className="hero_github">
+                    Github
+                </a>
+                <a href="https://www.linkedin.com/in/nathan-thamtoro-312156348" target="_blank" rel="noopener noreferrer" className="hero_Linkdin">
+                    Linkedin
+                </a>
+                <a href="https://www.instagram.com/nathan_thamtoro/?hl=en" target="_blank" rel="noopener noreferrer" className="hero_Instagram">
+                    Instagram
+                </a>
+                <a href="./public/Nathan-Thamtoro-Resume.pdf" target="_blank" rel="noopener noreferrer" className="hero_Resume">
+                    Resume
+                </a>
+            </div>
         </div>
     )
 
