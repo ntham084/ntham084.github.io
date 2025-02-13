71d61ba563c638c33545e0e4d45086df9919254c
ntham084 <nthamtoro@gmail.com>
2025-02-13 03:53:24 -0800

    Feat: Added favicon, fixed typos, and fixed home page picture

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './hero.css'
-import profile_img from '../../assets/ProfilePic.svg'
-
 
 const Hero = () => {
     return (
         <div id = "Home" className = 'hero'>
-            <img src= {profile_img} alt="Profile Picture of Nathan" />
+            <img src= "./public/profile.JPG" alt="Profile Picture of Nathan" />
             <h1><span>I'm Nathan Thamtoro,</span> Computer Science Student from Simon Fraser University</h1>
             <p>I am a aspiring software engineer ready to make the best of any oppurtunity given</p>
             <div className = "hero-action">
