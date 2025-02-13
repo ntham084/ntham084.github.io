e3df49e226beed20545b8465b256f3706757a1e6
ntham084 <nthamtoro@gmail.com>
2025-02-13 02:24:25 -0800

    Feat: Added button and container structure for buttons with background color

--- src/Hero/hero.jsx
+++ src/Hero/hero.jsx
@@ -10,16 +10,16 @@ const Hero = () => {
             <h1><span>I'm Nathan Thamtoro,</span> Computer Science Student from Simon Fraser University</h1>
             <p>I am a aspiring software engineer ready to make the best of any oppurtunity given</p>
             <div className = "hero-action">
-                <a href="https://github.com/ntham084" target="_blank" rel="noopener noreferrer" className="hero_github">
+                <a href="" target="_blank" rel="noopener noreferrer" className="hero_github">
                     Github
                 </a>
-                <a href="https://www.linkedin.com/in/nathan-thamtoro-312156348" target="_blank" rel="noopener noreferrer" className="hero_Linkdin">
+                <a href="" target="_blank" rel="noopener noreferrer" className="hero_Linkdin">
                     Linkedin
                 </a>
-                <a href="https://www.instagram.com/nathan_thamtoro/?hl=en" target="_blank" rel="noopener noreferrer" className="hero_Instagram">
+                <a href="" target="_blank" rel="noopener noreferrer" className="hero_Instagram">
                     Instagram
                 </a>
-                <a href="./public/Nathan-Thamtoro-Resume.pdf" target="_blank" rel="noopener noreferrer" className="hero_Resume">
+                <a href="" target="_blank" rel="noopener noreferrer" className="hero_Resume">
                     Resume
                 </a>
             </div>
