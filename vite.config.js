b8625bc18619d3144ac233b407c86c19ab314a58
ntham084 <nthamtoro@gmail.com>
2025-02-13 00:43:28 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: 'docs',
-  },
+  base: "/ntham084.github.io",
 })
