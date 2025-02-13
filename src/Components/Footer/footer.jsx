3f63864f9fb21e3ed2e987b255923b31ca509c56
ntham084 <nthamtoro@gmail.com>
2025-02-13 03:38:25 -0800

    Feat: Added footer divisions, added personal information and copyright statement

---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className = "footer">
+            <div className="footer-content">
+                <p>Email: nthamtoro@gmail.com</p>
+                <p>Phone Number: 778-814-8848</p>
+                <p>Copyright © 2025 Nathan Thamtoro</p>
+            </div>
+        </div>
+    )
+}
+export default Footer
