8d08086808b850c640eae006d983f21e5c39b695
ntham084 <nthamtoro@gmail.com>
2025-02-13 03:01:55 -0800

    Feat: Added divs for the project boxes. Boxes include title description and image of the projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Components/Navbar/navbar';
 import Hero from './Components/Hero/hero';
 import About from './Components/About/about';
+import Project from './Components/Projects/projects';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Project/>
     </div>
   )
 }
